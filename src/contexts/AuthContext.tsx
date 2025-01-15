c1b4742196daceefe23fa968d2bfe90a08a75899
MatheusMoorete <matheus.moorete@gmail.com>
2025-01-15 17:55:13 -0300

    Mudançça no redirect para o google

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -55,10 +55,10 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: window.location.origin
+        redirectTo: 'https://faltometro.netlify.app/api/auth/callback/google'
       }
     });
-
+  
     if (error) throw error;
   };
 
